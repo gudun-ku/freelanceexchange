7c49f83a847c1927fcff81b3a7bf349fd3f71ba4
Aleksandr Beloushkin <aleks.beloushkin@yahoo.com>
2019-12-16 21:36:20 +0300

    Adds new script and functions to make site work

---
+++ script/script.js
@@ -0,0 +1,51 @@
+document.addEventListener('DOMContentLoaded', () => {
+  'use strict';
+
+  const customer = document.getElementById('customer');
+  const freelancer = document.getElementById('freelancer');
+
+  const blockCustomer = document.getElementById('block-customer');
+  const blockFreelancer = document.getElementById('block-freelancer');
+  const blockChoice = document.getElementById('block-choice');
+  const btnExit = document.getElementById('btn-exit');
+  const formCustomer = document.getElementById('form-customer');
+
+  customer.addEventListener('click', () => {
+    //console.dir(blockCustomer);
+    blockFreelancer.style.display = 'none';
+    blockCustomer.style.display = 'block';
+    blockChoice.style.display = 'none';
+    btnExit.style.display = 'block';
+  });
+
+  freelancer.addEventListener('click', () => {
+    blockCustomer.style.display = '';
+    blockFreelancer.style.display = 'block';
+    blockChoice.style.display = 'none';
+    btnExit.style.display = 'block';
+  });
+
+  btnExit.addEventListener('click', () => {
+    blockFreelancer.style.display = 'none';
+    blockCustomer.style.display = 'none';
+    blockChoice.style.display = 'block';
+    btnExit.style.display = 'none';
+  });
+
+  formCustomer.addEventListener('submit', e => {
+    e.preventDefault();
+
+    const obj = {};
+
+    for (const elem of formCustomer.elements) {
+      if (
+        (elem.tagName === 'INPUT' && elem.type !== 'radio') ||
+        (elem.type === 'radio' && elem.checked)
+      ) {
+        obj[elem.name] = elem.value;
+      }
+    }
+
+    console.log(obj);
+  });
+});
